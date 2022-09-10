74878e91ae15a2aa696e839ed7b90f831f889030
KevinCarr42 <kevincarr@gmail.com>
2022-09-10 12:45:48 -0300

    finished id lookup in API

--- useful queries.sql
+++ useful queries.sql
@@ -6,3 +6,20 @@ WHERE artists.name LIKE 'zz top'
 
 -- Export to csv for lookup
 SELECT id FROM tracks
+
+
+-- very slow, even if you limit tracks
+SELECT * FROM tracks
+JOIN r_track_artist ON tracks.id = r_track_artist.track_id
+JOIN artists ON r_track_artist.artist_id = artists.id
+JOIN audio_features ON audio_features.id = tracks.audio_feature_id
+
+-- alternative without the junk (still slow)
+SELECT 
+    artists.name, tracks.name, 
+    acousticness, danceability, duration, energy, instrumentalness, key, 
+    liveness, loudness, mode, speechiness, tempo, time_signature, valence 
+    FROM tracks
+JOIN r_track_artist ON tracks.id = r_track_artist.track_id
+JOIN artists ON r_track_artist.artist_id = artists.id
+JOIN audio_features ON audio_features.id = tracks.audio_feature_id
